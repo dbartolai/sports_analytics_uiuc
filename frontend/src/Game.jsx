de992eefab97df2e826102d275eeeeaec81cbff5
dbartolai <drakeab2@illinois.edu>
2025-02-26 14:26:53 -0600

    Started on front end

---
+++ frontend/src/Game.jsx
@@ -0,0 +1,7 @@
+import './Game.css'
+
+function Game(away, home) {
+
+}
+
+export default Game
---
+++ frontend/src/Header.jsx
@@ -0,0 +1,23 @@
+import './App.css'
+
+function Header() {
+    return (
+        <>
+            <div className='header'>
+                <h1 className='title'> NBA Predictor</h1>
+                <div class='hotbar'>
+                    {/* Add hotbar links in here */}
+                    <a className='hotbar-link'>Link</a>
+                    <a className='hotbar-link'>Link</a>
+                    <a className='hotbar-link'>Link</a>
+                    <a className='hotbar-link'>Link</a>
+                    <a className='hotbar-link'>Link</a>
+                    <a className='hotbar-link'>Link</a>
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header
