85f2f7c58b3eed88bb6053dd32ffa161c3d5bf36
djcarp04 <djcarp04@gmail.com>
2025-02-28 16:33:55 -0600

    first commit

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Header from './Header.jsx'
+import DanielC from './DanielC'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Header />
     <App />
+    <DanielC />
   </StrictMode>,
 )
