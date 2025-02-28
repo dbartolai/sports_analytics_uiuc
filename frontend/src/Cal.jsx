407db2225ad7e583098f6d20010e26674fd3af1c
calumiv2 <calumiv2@illinois.edu>
2025-02-28 15:59:10 -0600

    Cal adding his item

---
+++ frontend/src/Cal.jsx
@@ -0,0 +1,14 @@
+import './Cal.css'
+
+function Cal() {
+
+    return (
+      <>
+        <div className='bluetext'>
+            <h1>I hope this works</h1>
+        </div>
+      </>
+    )
+  }
+
+  export default Cal
