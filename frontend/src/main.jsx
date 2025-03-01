607652f3d066d29c85019cc3fc06685e0cf71fa3
dbartolai <drakeab2@illinois.edu>
2025-02-28 21:21:30 -0600

    merge

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Header from './Header.jsx'
+import Drake from './Drake.jsx'
+import Cal from './Cal.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
