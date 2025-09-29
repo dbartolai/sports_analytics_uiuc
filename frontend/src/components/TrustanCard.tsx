9ea36ac65a5306fefd037bd44afbf6f1ab1367f6
trustangabrielprice <tpric5@illinois.edu>
2025-09-28 19:10:03 -0500

    creating branch and card

---
+++ frontend/src/components/TrustanCard.tsx
@@ -0,0 +1,11 @@
+function TrustanCard () {
+    return (
+        <div>
+            <h3>Trustan Price</h3>
+            <h6>Statistics</h6>
+            <h6>Basketball</h6>
+        </div>
+    )
+}
+
+export default TrustanCard
